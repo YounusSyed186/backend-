2338035d9107e39863e46f61bb03bd6ffef04e4d
YounusSyed186 <younussyed1011@gmail.comgit config --global user.email younussyed1011@gmail.com>
2025-04-29 14:32:02 +0530

    Add models for comments, likes, playlists, and tweets with appropriate schemas

---
+++ src/models/playlist.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose'
+
+const playlistSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        discription: {
+            type: String,
+            required: true
+        },
+        video: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+        }],
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+
+    }, { timestamps: true }
+
+)
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
